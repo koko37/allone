ac65fd768872ca3b311a1285f7c4eedc8659bb8b
Engel <engel0088@gmail.com>
2014-02-03 10:09:57 +0800

    remove old capi

--- config/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-set :stage, :production
-set :branch, :master
-
-server "allone-solution.net", user: 'dev', roles: %w{:app, :web, :db}, :primary => true, :password=>'mouse1122'
-set :deploy_to, "/home/dev/app/allone/ovida"
-
-set :rails_env, :production
--- config/deploy/staging.rb
+++
@@ -1,2 +0,0 @@
-server "allone-solution.net", :app, :web, :db, :primary => true
-set :deploy_to, "/home/dev/app/allone/ovida_staging"
