67edbcd3ef7ed24543e52f47afcf45e68cf5ab72
Engel <engel0088@gmail.com>
2014-02-03 10:03:14 +0800

    capi

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,20 @@
-require 'capistrano/ext/multistage'
-
 lock '3.1.0'
 
 set :application, "ovida"
-set :scm, :git
-set :repository, "git@bitbucket.org:gototome/ovida.git"
-set :scm_passphrase, "mouse1122"
-set :user, "dev"
+set :deploy_user, "dev"
 
-set :stages, ["staging","production"]
-set :default_stage, "staging"
+set :scm, :git
+set :scm_password, "mouse1122"
+
+set :repo_url, "git@bitbucket.org:gototome/ovida.git"
+
+#set :rbenv_type, :system
+#set :rbenv_ruby, '1.9.3p484'
+
+set :keep_releases, 5
+
+set :linked_files, %w{config/database.yml}
+
+namespace :deploy do
+
+end
